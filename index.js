078915972661a8139f84b3e27032f8c2954752d8
RuiSiang <ruisiang2000@gmail.com>
2023-10-16 11:02:25 +0800

    add create page

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 const fs = require('fs-extra')
 const dotenv = require('dotenv')
+const path = require('path')
+const bodyParser = require('koa-bodyparser');
 
-const Koa = require('koa');
-const Router = require('@koa/router');
+const Koa = require('koa')
+const Router = require('@koa/router')
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
+app.use(bodyParser());
+
 const urlMap = JSON.parse(fs.readFileSync('urls.json', 'utf-8'))
-const template = fs.readFileSync('render.html', 'utf-8')
+const newTemplate = fs.readFileSync(path.join(process.cwd(), 'render', 'new.html'), 'utf-8')
+const externalTemplate = fs.readFileSync(path.join(process.cwd(), 'render', 'external.html'), 'utf-8')
 
 dotenv.config()
 
 router.get('/_', (ctx, next) => {
-  console.log('test')
-});
+  ctx.body = newTemplate
+})
 
-router.get(/^\/\_([a-kmnp-zA-HJ-Z2-9]{4})/, (ctx, next) => {
+const chars = 'abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789';
+
+const genRndStr = () => {
+  let result = '';
+  const charLength = chars.length;
+  for (let i = 0; i < 4; i++) {
+    result += chars.charAt(Math.floor(Math.random() * charLength));
+  }
+  return result;
+}
+
+router.post('/_', (ctx, next) => {
+  if (ctx.request.body && ctx.request.body['auth'] && ctx.request.body['auth'] == process.env.AUTH) {
+    const rndStr = genRndStr()
+    urlMap[rndStr] = ctx.request.body['url']
+    fs.writeFileSync('urls.json', JSON.stringify(urlMap), { charset: 'utf-8' })
+    ctx.redirect(`/_${rndStr}`)
+  } else {
+    ctx.status = 403
+    ctx.body = 'Unauthorized'
+  }
+})
+
+router.get(/^\/\_([a-kmnp-zA-HJ-NP-Z2-9]{4})/, (ctx, next) => {
   const token = ctx.url.slice(2)
   if (urlMap[token]) {
-    ctx.body = template.replace(/\{\{url\}\}/g, urlMap[token])
+    ctx.body = externalTemplate.replace(/\{\{url\}\}/g, urlMap[token])
   }
-});
+})
 
 app
   .use(router.routes())
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
 
 const port = parseInt(process.env.PORT || '3000')
-app.listen(port);
+app.listen(port)
 console.log(`URL shortener listening on port ${port}`)
